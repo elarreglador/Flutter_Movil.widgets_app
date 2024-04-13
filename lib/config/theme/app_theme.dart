d89f12c61d5aff3ca673eae4b712badd091f2f7b
David M <elarreglador@protonmail.com>
2024-04-13 18:16:36 +0200

    Tema y estilos de la aplicacion

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.yellow,
+  Colors.pink,
+  Colors.black,
+  Colors.amber,
+  Colors.cyan,
+  Colors.deepOrange,
+  Colors.grey,
+  Colors.indigo
+];
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }):
+  assert (selectedColor >= 0,  
+    "Selected color must be greaher than 0"),
+  assert (selectedColor <= colorList.length, 
+    "Selected color must be less or equal than ${colorList.length-1}");
+  
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList [ selectedColor ]
+  );
+
+}
