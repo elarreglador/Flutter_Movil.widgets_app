657bd3b1f092ec789bbd3f08e48ae71e3e3bcb8d
David M <elarreglador@protonmail.com>
2024-04-18 07:19:33 +0200

    go_router: rutas con nombres

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 
 class HomeScreen extends StatelessWidget {
+
+  static const String name = "home_screen";
+
   const HomeScreen({
     super.key
   });
@@ -58,6 +62,7 @@ class _CustomListTile extends StatelessWidget {
       onTap: () { 
         // SUPERPONE UNA NUEVA PANTALLA
         context.push( menuItem.link );
+        // context.push( CardsScreen.name ); //alternativa a link
       },
     );
   }
