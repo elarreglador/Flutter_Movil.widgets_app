70ac90093d662d9eacc0766ca13647898ad79820
David M <elarreglador@protonmail.com>
2024-04-28 17:47:45 +0200

    Animado el contenedor animado

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   
   static const name = 'animated_screen';
   
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+
+  double width = 250;
+  double height = 450;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changeShape(){
+    final random = Random();
+    // anncho entre 50 y 350
+    width = random.nextInt(200)+120;
+    // alto entre 50 y 700
+    height = random.nextInt(300)+120;
+    // color aleatorio
+    color = Color.fromRGBO(
+      random.nextInt(255), //R
+      random.nextInt(255), //G
+      random.nextInt(255), //B
+      1 //Opacity
+    );
+    // radio de las esquinas
+    borderRadius = random.nextInt(100)+20;
+
+    // actuliza el estado de la figura
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,18 +50,18 @@ class AnimatedScreen extends StatelessWidget {
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 200), // duracion de la nimacion
           curve: Curves.easeOutCubic, // tipo de animacion
-          width: 200,
-          height: 100,
+          width: width,
+          height: height,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20)
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius)
           ),
         ),
       ),
 
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-
+          changeShape();
         },
         child: const Icon(Icons.play_arrow_outlined),
       ),
