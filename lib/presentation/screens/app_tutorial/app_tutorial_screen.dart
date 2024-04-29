b38320e4e7c513ef69e95fdd2a87ca768574e737
David M <elarreglador@protonmail.com>
2024-04-29 18:21:35 +0200

    agrego boton salir, y el de comenzar a partir de un controller

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -17,11 +17,42 @@ final slides = <SlideInfo>[
 ];
 
 
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
   
   static const name = 'tutorial_screen';
   
   const AppTutorialScreen({super.key});
+
+  @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+
+  final PageController pageviewController = PageController();
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageviewController.addListener((){
+      final page = pageviewController.page ?? 0;
+      if (!endReached && page >= (slides.length -1.5)){
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+
+  @override
+  void dispose(){
+    pageviewController.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +64,7 @@ class AppTutorialScreen extends StatelessWidget {
         children: [
 
           PageView(
+            controller: pageviewController,
             physics: const BouncingScrollPhysics(),
             children: slides.map(
               (slideData) => _Slide(
@@ -49,8 +81,17 @@ class AppTutorialScreen extends StatelessWidget {
             child: TextButton(
               onPressed: () => context.pop(), 
               child: const Text("Salir >>",))
-            )
+          ),
 
+          endReached ?
+            Positioned(
+              right: 100,
+              bottom: 50,
+              child: FilledButton(
+                onPressed: () => context.pop(),
+                child: const Text("Comenzar"),
+              )
+            ): const SizedBox()
         ],
       ),
     );
